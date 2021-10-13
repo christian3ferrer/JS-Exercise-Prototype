23639def59a072b9f2f7039b9c7b5c06f5011748
christian3ferrer <88175259+christian3ferrer@users.noreply.github.com>
2021-10-13 18:19:06 -0400

    finished

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ Car.prototype.drive =function(dist){
     - Write a Baby constructor subclassing Person.
     - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
-        + Should return a string "Playing with x", x being the favorite toy.
+        + Should return a string "Playing with x", x being the favorite toy...
 */
 function Baby(name,age,favoriteToy) {
  Person.call (this, name, age);
